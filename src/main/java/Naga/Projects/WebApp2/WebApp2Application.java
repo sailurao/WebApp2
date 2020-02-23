1519fe3e85c3e4d09fc816a80afccd89d2b2c252
Naga <sailurao@yahoo.com>
2020-02-23 15:55:12 -0500

    Initial commit

---
+++ src/main/java/Naga/Projects/WebApp2/WebApp2Application.java
@@ -0,0 +1,13 @@
+package Naga.Projects.WebApp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/Naga/Projects/WebApp2/WebApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package Naga.Projects.WebApp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
